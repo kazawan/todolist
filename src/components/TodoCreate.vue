09a7b4daf9f50ef2ebeb0621c46975b5f15266e7
kazawan <55086403@qq.com>
2024-03-04 01:20:24 +0800

    modified:   src/components/TodoCreate.vue 	modified:   src/components/TodoUpdate/TodoUpdate.vue

--- src/components/TodoCreate.vue
+++ src/components/TodoCreate.vue
@@ -1,8 +1,8 @@
 <template>
     <div class="w-[80%] bg-slate-100 rounded-md shadow-md">
         <div class="flex justify-between items-center p-2">
-            <h1 class="text-2xl font-bold">Create Todo</h1>
-            <button @click.stop="removeModal">Close</button>
+            <h1 class="text-lg font-bold  bg-green-400 px-2 rounded-md shadow-md select-none">Create Todo</h1>
+            <button class="text-sm  bg-red-500/50 text-gray-900 px-2 rounded-md h-8 shadow-md  hover:bg-red-500" @click.stop="removeModal">Close</button>
         </div>
         <div class="p-2 " @click.stop>
             <form @submit.stop.prevent="createTodo">
@@ -10,13 +10,13 @@
                     <input
                         type="text"
                         v-model="title"
-                        class="p-2 border-2 border-gray-300"
+                        class="p-2 border-2 border-gray-300 rounded-md"
                         placeholder="Title"
                     />
 
                     <button
                         type="submit"
-                        class="p-2 bg-slate-200 text-white font-bold"
+                        class="p-2 bg-slate-200 text-white font-bold rounded-md hover:bg-slate-600 transition-all duration-300 ease-in-out"
                     >
                         Create
                     </button>
--- src/components/TodoUpdate/TodoUpdate.vue
+++ src/components/TodoUpdate/TodoUpdate.vue
@@ -2,8 +2,8 @@
   <div class="Modal" @click="removeModal">
     <div class="w-[80%] bg-slate-100 rounded-md shadow-md">
       <div class="flex justify-between items-center p-2">
-        <h1 class="text-2xl font-bold">Update Todo</h1>
-        <button @click.stop="removeModal">Close</button>
+        <h1 class="text-lg font-bold  bg-green-400 px-2 rounded-md shadow-md select-none">Update Todo</h1>
+        <button class="text-sm  bg-red-500/50 text-gray-900 px-2 rounded-md h-8 shadow-md  hover:bg-red-500" @click.stop="removeModal">Close</button>
       </div>
       <div class="p-2" @click.stop>
         <form @submit.stop.prevent="updateT">
@@ -11,11 +11,11 @@
             <input
               type="text"
               v-model="title"
-              class="p-2 border-2 border-gray-300"
+              class="p-2 border-2 border-gray-300 rounded-md"
               :placeholder="placeHolder"
             />
 
-            <button type="submit" class="p-2 bg-slate-200 text-white font-bold">
+            <button type="submit" class="p-2 bg-slate-200 text-white font-bold rounded-md hover:bg-slate-600 transition-all duration-300 ease-in-out">
               Update
             </button>
           </div>
