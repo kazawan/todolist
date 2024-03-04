47499245d3206c9a63692b7f3c675f92f673fba1
kazawan <55086403@qq.com>
2024-03-04 13:21:26 +0800

    修改了定位问题:   src/components/TodoNotices/TodoNotices.vue

--- src/components/TodoNotices/TodoNotices.vue
+++ src/components/TodoNotices/TodoNotices.vue
@@ -1,10 +1,10 @@
 <template>
-    <div v-show="props.notice"  class="absolute top-0 left-0 w-full h-screen p-2  flex  z-10  justify-center ">
-        <div class="w-64  h-fit p-2 bg-gray-300/30 rounded-md text-[.8rem] border-2 border-gray-800/50  backdrop-blur-sm">
+    <!-- <div v-show="props.notice"  class="absolute top-0 left-0 w-full h-screen p-2  flex  z-10  justify-center "> -->
+        <div class=" fixed top-10  translate-x-[-50%]  left-[50%] w-64  h-fit p-2 bg-gray-300/30 rounded-md text-[.8rem] border-2 border-gray-800/50  backdrop-blur-sm">
             {{ props.msg }}  
         </div>
       
-    </div>
+    <!-- </div> -->
     
 </template>
 
